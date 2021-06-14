5e08239ffd4c17decc4f002be9f15319e6706972
heyarne <hi@arnes.space>
2021-06-14 11:34:06 +0200

    Add env vars to shell.nix for JOGL

--- shell.nix
+++ shell.nix
@@ -6,4 +6,12 @@ pkgs.mkShell rec {
     xorg_sys_opengl
   ];
   LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
+
+  # we need to make sure the library is on the path for JOGL;
+  # also, there's a bug that is avoided with the second config line
+  # https://github.com/processing/processing/issues/5476
+  JAVA_OPTS = "-Djava.library.path=${lib.makeLibraryPath buildInputs}"; 
+    # -Djogl.disable.openglcore=\"true\"
+  # LIBGL_ALWAYS_SOFTWARE = true;
+  # JAVA_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
 }
